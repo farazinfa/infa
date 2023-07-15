eb341766f202eb9cab8b599d93100ea5dea9b516
farainfa <farazfan123@gmail.com>
2023-07-15 18:51:30 +0530

    firstcommit

---
+++ src/main/java/com/example/infa/InfaApplication.java
@@ -0,0 +1,13 @@
+package com.example.infa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/infa/InfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
